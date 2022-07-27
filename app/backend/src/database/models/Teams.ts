da7b0a9a8f7ab28a1a0d412393b11a420ffe08ea
obeaune <linecora86@gmail.com>
2022-07-27 20:43:50 -0300

    feat: create model and migration for `Teams`

---
+++ app/backend/src/database/models/Teams.ts
@@ -0,0 +1,27 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Teams extends Model {
+  id!: number;
+  teamName!: string;
+}
+
+Teams.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING(50),
+    allowNull: false,
+    field: 'team_name',
+  },
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  underscored: true,
+  timestamps: false });
+
+export default Teams;
